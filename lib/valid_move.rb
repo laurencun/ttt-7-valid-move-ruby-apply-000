d95dbcc1174ae4aeed90489b32870aaec302ce4c
Lauren Cunningham <lauren.busbee@gmail.com>
2020-05-16 05:02:35 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken? true
-    return false
+  if position_taken? == true
+    puts
   else 
     return true
 end    
