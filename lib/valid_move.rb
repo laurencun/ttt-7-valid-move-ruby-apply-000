68f59d221db7a3552b2af3da64786318e63cc5c7
Lauren Cunningham <lauren.busbee@gmail.com>
2020-05-16 05:00:17 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  
-  
+  if position_taken? == true
+    return false
+  else 
+    return true
 end    
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
